6829ef195eaed0228a83f0f987bc8eecd8efff32
Scott Bellware <sbellware@gmail.com>
2023-03-21 13:56:59 -0500

    Write messages control is corrected

--- exercises/01-write-message.sql
+++ exercises/01-write-message.sql
@@ -2,5 +2,5 @@ SELECT write_message(
   '00000001-ffff-ffff-ffff-ffffffffffff',
   'someStreamName',
   'SomeMessageType',
-  '{"some_attribute": "some value"}'
+  '{"someAttribute": "some value"}'
 );
